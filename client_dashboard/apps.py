4a3565fba2eda651340d471427925a2c6c15ac8c
Mh-Osman <osmangani1osm@gmail.com>
2025-11-04 00:42:53 +0600

    i added notification for checkout form

--- client_dashboard/apps.py
+++ client_dashboard/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class ClientDashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'client_dashboard'
+    def ready(self):
+        import client_dashboard.signals
---
+++ client_dashboard/signals.py
@@ -0,0 +1,43 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import ClientCheckoutForm
+from notifications.models import Notification
+
+@receiver(post_save, sender=ClientCheckoutForm)
+def create_client_checkout_form(sender, instance, created, **kwargs):
+    if not created:
+        return
+
+    client = instance.client
+
+    # Notify subscription employees
+    if instance.subscription:
+        for emp in instance.subscription.employee.all():
+            message = f"New Client Checkout Form '{instance.form_name}' created by {client.name}."
+            Notification.objects.create(
+                title="New Client Checkout Form",
+                for_user=emp,   # not emp_id
+                message=message,
+                for_admin=False,
+                for_all=False
+            )
+
+    # Notify special service worker
+    if instance.special_service:
+        emp = instance.special_service.worker
+        message = f"New Client Checkout Form '{instance.form_name}' created by {client.name}."
+        Notification.objects.create(
+            title="New Client Checkout Form",
+            for_user=emp,
+            message=message,
+            for_admin=False,
+            for_all=False
+        )
+
+    # Notify admin
+    Notification.objects.create(
+        title="New Client Checkout Form Created",
+        message=f"A new Client Checkout Form '{instance.form_name}' has been created by {client.name}. Please review it.",
+        for_admin=True,
+        for_all=False
+    )
